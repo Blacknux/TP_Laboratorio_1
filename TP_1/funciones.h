1affe40c0419c670ed29588fd18081b4963d2b43
Nahuel <nahuel.claret@gmail.com>
2016-03-20 17:47:52 -0300

    Agregue punto 1 2 3 4 6 8 del tp1

--- TP_1/funciones.h
+++ TP_1/funciones.h
@@ -4,3 +4,42 @@
 
 
 #endif // FUNCIONES_H_INCLUDED
+/**
+*\brief Recibe los dos operandos y los suma
+*/
+
+int sumar(int x, int y)
+{
+    int suma;
+    suma=x+y;
+    printf("El resultado de la suma de %d y %d es: %d\n",x,y,suma);
+
+
+}
+
+/**
+*\brief Recibe los dos operandos y los resta
+*/
+
+int restar(int x, int y)
+{
+    int resta;
+    resta=x-y;
+    printf("El resultado de la resta de %d y %d es: %d\n",x,y,resta);
+
+
+}
+
+/**
+*\brief Recibe los dos operandos y los multiplica
+*/
+
+
+int multiplicar(int x, int y)
+{
+    int multiplica;
+    multiplica=x*y;
+    printf("El resultado de la multiplicacion de %d y %d es: %d\n",x,y,multiplica);
+
+
+}
--- TP_1/main.c
+++ TP_1/main.c
@@ -6,11 +6,13 @@ int main()
 {
     char seguir='s';
     int opcion=0;
+    int x=0;
+    int y=0;
 
     while(seguir=='s')
     {
-        printf("1- Ingresar 1er operando (A=x)\n");
-        printf("2- Ingresar 2do operando (B=y)\n");
+        printf("1- Ingresar 1er operando (A= %i)\n"),x;
+        printf("2- Ingresar 2do operando (B= %i)\n"),y;
         printf("3- Calcular la suma (A+B)\n");
         printf("4- Calcular la resta (A-B)\n");
         printf("5- Calcular la division (A/B)\n");
@@ -24,25 +26,43 @@ int main()
         switch(opcion)
         {
             case 1:
+                printf("Ingrese el 1er operando: ");
+                scanf("%d",&x);
+                printf("\n\n");
                 break;
             case 2:
+                printf("Ingrese el 2do operando: ");
+                scanf("%d",&y);
+                printf("\n\n");
                 break;
             case 3:
+                sumar(x,y);
+                printf("\n\n");
                 break;
             case 4:
+                restar(x,y);
+                printf("\n\n");
                 break;
             case 5:
                 break;
             case 6:
+                multiplicar(x,y);
                 break;
             case 7:
                 break;
             case 8:
+                sumar(x,y);
+                printf("\n\n");
+                restar(x,y);
+                printf("\n\n");
+                multiplicar(x,y);
+                printf("\n\n");
                 break;
             case 9:
                 seguir = 'n';
                 break;
         }
+    }
 
 
     return 0;
